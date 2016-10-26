de79b38808b0ae2fef2d9facfbea8a57fc920d5b
plsb <pedroluissaraiva2@hotmail.com>
2016-10-26 21:57:53 -0200

    commit de nova aplicação

---
+++ prj_poo1/src/main/java/br/com/poo/PrjPoo1Application.java
@@ -0,0 +1,12 @@
+package br.com.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjPoo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjPoo1Application.class, args);
+	}
+}
